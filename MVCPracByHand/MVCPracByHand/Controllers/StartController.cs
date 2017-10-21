2e861189eef724dce6f70e15fba971f654460fb1
csharpspecialist <netspec1@hotmail.com>
2017-10-21 15:39:21 -0400

    added html template landing page to MVC webpage...works great

---
+++ MVCPracByHand/MVCPracByHand/Controllers/StartController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPracByHand.Controllers
+{
+    public class StartController : Controller
+    {
+        // GET: Start
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Start
+        public ActionResult What()
+        {
+            return View();
+        }
+
+
+        public ActionResult Spec()
+        {
+            return View();
+        }
+
+        public ActionResult Go()
+        {
+            return View();
+        }
+
+        public ActionResult Pro()
+        {
+            return View();
+        }
+
+    }
+}
